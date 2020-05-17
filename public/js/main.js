930f902dfc95572c995da2aaf5db01a3e18bb94c
XG <xiang2@uci.edu>
2020-05-16 23:01:51 -0400

    lobby updates

--- public/js/main.js
+++ public/js/main.js
@@ -17,22 +17,100 @@ if('undefined' == typeof username || !username){
     username = 'Anonymous_'+Math.random();
 }
 
-var chat_room = 'One_Room'
+var chat_room = getURLParameters('game_id');
+if('undefined' == typeof chat_room || !chat_room){
+    chat_room = 'lobby';
+}
 
 /* connect to socket server */
 var socket = io.connect();
 
+/* when server sends me log msg */
 socket.on('log',function(array){
     console.log.apply(console,array);
 });
 
+/* when server responds that someone joined room */
 socket.on('join_room_response',function(payload){
 if(payload.result == 'fail'){
     alert(payload.message);
     return;
 }
-$('#messages').append('<p>New user joined the room: '+payload.username+'</p>');
+
+/* if we joined room, ignore */
+if(payload.socket_id == socket.id) {
+    return;
+}
+
+/* if someone joined, add new row to lobby table */
+var dom_elements = $('.socket_'+payload.socket_id);
+
+/* if we don't have entry for person */
+if(dom_elements.length == 0){
+    var nodeA = $('<div></div>');
+    nodeA.addClass('socket_'+payload.socket_id);
+    var nodeB = $('<div></div>');
+    nodeB.addClass('socket_'+payload.socket_id);
+    var nodeC = $('<div></div>');
+    nodeC.addClass('socket_'+payload.socket_id);
+
+    nodeA.addClass('w-100');
+    nodeB.addClass('col-9 text-right');
+    nodeB.append('<h4>'+payload.username+'</h4>');
+    nodeC.addClass('col-3 text-left');
+    var buttonC = makeInviteButton();
+    nodeC.append(buttonC);
+
+    nodeA.hide();
+    nodeB.hide();
+    nodeC.hide();
+    $('#players').append(nodeA,nodeB,nodeC);
+    nodeA.slideDown(1000);
+    nodeB.slideDown(1000);
+    nodeC.slideDown(1000);
+}
+else{
+    var buttonC = makeInviteButton();
+    $('.socket_'+payload.socket_id+' button').replaceWith(buttonC);
+    dom_elements.slideDown(1000);
+}
+
+/* new player joined msg */
+var newHTML = '<p>'+payload.username+' just entered the lobby</p>';
+var newNode = $(newHTML);
+newNode.hide();
+$('#messages').append(newNode);
+newNode.slideDown(1000);
 });
+
+
+/* when server responds that someone left room */
+socket.on('player_disconnected',function(payload){
+    if(payload.result == 'fail'){
+        alert(payload.message);
+        return;
+    }
+    
+    /* if we left room, ignore */
+    if(payload.socket_id == socket.id) {
+        return;
+    }
+    
+    /* if someone left, animate out their content */
+    var dom_elements = $('.socket_'+payload.socket_id);
+    
+    /* if something exists */
+    if(dom_elements.length != 0){
+        dom_elements.slideUp(1000);
+    }
+    
+    /* player left msg */
+    var newHTML = '<p>'+payload.username+' has left the lobby</p>';
+    var newNode = $(newHTML);
+    newNode.hide();
+    $('#messages').append(newNode);
+    newNode.slideDown(1000);
+    });
 
 socket.on('send_message_response',function(payload){
     if(payload.result == 'fail'){
@@ -49,6 +127,12 @@ payload.username = username;
 payload.message = $('#send_message_holder').val();
 console.log('*** Client Log Message: \'send_message\' payload: '+JSON.stringify(payload));
 socket.emit('send_message',payload);
+}
+
+function makeInviteButton(){
+    var newHTML = '<button type=\'button\' class =\'btn btn-outline-primary\'>Invite</button>';
+    var newNode = $(newHTML);
+    return(newNode);
 }
 
 $(function(){
--- server.js
+++ server.js
@@ -31,9 +31,14 @@ var app = http.createServer(
 console.log('The server is running');
 
 /* *** set up web socket file server *** */
+
+/* registry of socket ids and player info */
+var players = [];
+
 var io = require('socket.io').listen(app);
 
 io.sockets.on('connection', function (socket) {
+    log('Client connection by '+socket.id);
     function log(){
         var array = ['*** Server Log Message: '];
         for(var i = 0; i < arguments.length; i++){
@@ -43,15 +48,13 @@ io.sockets.on('connection', function (socket) {
         socket.emit('log',array);
         socket.broadcast.emit('log',array);
     }
-        log('A website connected to the server');
     
-    socket.on('disconnect',function(socket){
-        log('A website disconnected from the server');
-    });
 
-/*join_room command */
+/* join_room command */
     socket.on('join_room',function(payload){
-        log('server received a command','join_room',payload);
+        log('\'join_room\' command'+JSON.stringify(payload));
+
+/* check client sent payload */
 if(('undefined' === typeof payload) || !payload){
     var error_message = 'join_room had no payload, command aborted';
     log(error_message);
@@ -61,6 +64,8 @@ if(('undefined' === typeof payload) || !payload){
     });
     return;
     }
+
+/* check payload has room to join */
 var room = payload.room;
 if(('undefined' === typeof room) || !room){
     var error_message = 'join_room didn\'t specify a room, command aborted';
@@ -71,6 +76,8 @@ if(('undefined' === typeof room) || !room){
     });
     return;
 }
+
+/* check username provided */
     var username = payload.username;
     if(('undefined' === typeof username) || !username){
         var error_message = 'join_room didn\'t specify a username, command aborted';
@@ -81,27 +88,56 @@ if(('undefined' === typeof room) || !room){
         });
         return;
     }
+
+    /* store info for new player */
+    players[socket.id] = {};
+    players[socket.id].username = username;
+    players[socket.id].room = room;
+
+    /* have user join room */
     socket.join(room);
 
+ /* get room object */
     var roomObject = io.sockets.adapter.rooms[room]; 
-    if(('undefined' === typeof roomObject) || !roomObject){
-        var error_message = 'join_room couldn\'t create a room (internal error), command aborted';
-        log(error_message);
-        socket.emit('join_room_response', {
-            result: 'fail',
-            message: error_message
-        });
-        return;
-    }
+    
+    /* tell everyone in room that someone joined */
 var numClients = roomObject.length;
 var success_data = {
     result: 'success',
     room: room,
     username: username,
-    membership: (numClients + 1)
+    socket_id: socket.id,
+    membership: numClients
 };
-io.sockets.in(room).emit('join_room_response',success_data);
-log('Room ' + room + ' was just joined by '+ username);
+io.in(room).emit('join_room_response',success_data);
+
+for(var socket_in_room in roomObject.sockets){
+    var success_data = {
+    result: 'success',
+    room: room,
+    username: players[socket_in_room].username,
+    socket_id: socket_in_room,
+    membership: numClients
+};
+socket.emit('join_room_response',success_data);
+}
+log('join_room success');
+    });
+
+
+
+    socket.on('disconnect',function(){
+        log('Client disconnected '+JSON.stringify(players[socket.id]));
+        if('undefined' !== typeof players[socket.id] && players[socket.id]){
+            var username = players[socket.id].username;
+            var room = players[socket.id].room;
+            var payload = {
+                username: username,
+                socket_id: socket.id
+            };
+            delete players[socket.id];
+            io.in(room).emit('player_disconnected',payload);
+        }
     });
 
 /* send_message command */
